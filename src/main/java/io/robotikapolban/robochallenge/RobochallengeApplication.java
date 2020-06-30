e140adebdeffeffac5bc3a7d67a1c6ca46979c5f
astro-rizki <34902434+astro-rizki@users.noreply.github.com>
2020-06-30 17:49:14 +0700

    Add files via upload

---
+++ src/main/java/io/robotikapolban/robochallenge/RobochallengeApplication.java
@@ -0,0 +1,13 @@
+package io.robotikapolban.robochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobochallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobochallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/robotikapolban/robochallenge/RobochallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.robotikapolban.robochallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobochallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
